64f2d53b21fd9708235201d0f4779ab58bdc411c
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-24 19:34:09 -0500

    Add backend from Spring Initializer

---
+++ backend/src/main/java/com/revature/ecomproject/EComProjectApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ecomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/revature/ecomproject/EComProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ecomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
