b5e889c6ae84449836fd9b73fd031e2957fbe933
Biruk <biruk@Biruks-MBP-2.fios-router.home>
2025-02-26 23:49:51 -0500

    Add createdAt and updatedAt fields, add Annotations

--- backend/src/main/java/com/revature/ecomproject/model/Category.java
+++ backend/src/main/java/com/revature/ecomproject/model/Category.java
@@ -1,30 +1,51 @@
 package com.revature.ecomproject.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Component
+@Entity
+@Table (name="category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int categoryId;
+
+    @Column(nullable = false)
     private String categoryName;
+
     private String image;         //Url for the picture
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 
     private List<Product> products; // One category has many products... someone who works on the product should add Product model
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
 
 
     public Category() {
 
     }
 
-    public Category(int categoryId, String categoryName, String image, List<Product> products) {
+    public Category(int categoryId, String categoryName, String image, List<Product> products, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
         this.image = image;
         this.products = products;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public int getCategoryId() {
@@ -59,6 +80,22 @@ public class Category {
         this.products = products;
     }
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
     @Override
     public String toString() {
         return "Category{" +
@@ -66,6 +103,8 @@ public class Category {
             ", categoryName='" + categoryName + '\'' +
             ", image='" + image + '\'' +
             ", products=" + products +
+            ", createdAt=" + createdAt +
+            ", updatedAt=" + updatedAt +
             '}';
     }
 }
