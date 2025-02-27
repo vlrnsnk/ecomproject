35be0e7813cd8bed2c3f57a7630431f7f3c6c5a6
Biruk <biruk@Biruks-MBP-2.fios-router.home>
2025-02-26 23:11:40 -0500

    Product model class is created

--- backend/src/main/java/com/revature/ecomproject/model/Category.java
+++ backend/src/main/java/com/revature/ecomproject/model/Category.java
@@ -4,6 +4,8 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 
+import java.util.List;
+
 public class Category {
 
     private int categoryId;
